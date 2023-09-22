349681e0dc419889c767e8329863aadab6f58d38
Amir Nikpour <amnik.free@gmail.com>
2023-09-22 20:52:05 +0330

    chat: add chat app to settings and url

--- LeLuChat_ChatEngine/settings.py
+++ LeLuChat_ChatEngine/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework.authtoken',
-    'users'
+    'users',
+    'chat'
 ]
 
 REST_FRAMEWORK = {
